ca6220717f0ab48f4c5e66588c642c1997d45f7d
HosseinSedghi <HosseinSedghiProgrammer@gmail.com>
2023-08-25 15:09:30 +0430

    create SiteSettings model

--- blog/models.py
+++ blog/models.py
@@ -3,7 +3,15 @@ from django.db import models
 # Create your models here.
 
 
-class SliderHome(models):
+class SliderHome(models.Model):
     image = models.ImageField(upload_to='images/slider_images')
 
+
+class SiteSettings(models.Model):
+    site_name = models.CharField(max_length=20)
+    about_site = models.TextField(null=True, blank=True)
+    email = models.EmailField()
+    fax = models.CharField(max_length=20)
+    phone = models.CharField(max_length=20)
+    address = models.CharField(max_length=80)
 
